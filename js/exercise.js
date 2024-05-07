0a4cf7b1a2b7a3296e3d4eb012db42dfbaf857b8
zjw2018SS <116158532+zjw2018SS@users.noreply.github.com>
2024-05-08 00:25:13 +0800

    Add files via upload

--- js/exercise.js
+++ js/exercise.js
@@ -502,7 +502,10 @@ function json2paper(json, is_order = "0") {
     drop_box.style.display = "none"
 
     let sheet = document.getElementById("sheet")
-    sheet.style.visibility = "hidden"
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))){
+        sheet.style.visibility = "hidden"
+    }
+
     sheet.style.display = "block"
 
     json_body = sort_json(json_body, is_order)
--- js/exerciseM.js
+++ js/exerciseM.js
@@ -3,13 +3,29 @@ nav_center.addEventListener("click", nav_center_display);
 
 function nav_center_display() {
     let nav_center = document.getElementById("nav_center");
+    let main=document.getElementById("main");
     let is_nav_center_display = nav_center.dataset.isNavCenterDisplay
     let sheet = document.getElementById("sheet")
     if (is_nav_center_display == "0") {
         sheet.style.visibility = "visible"
+        // scrControl(0, main)
     } else if (is_nav_center_display == "1") {
         sheet.style.visibility = "hidden"
+        // scrControl(1, main)
     }
     nav_center.dataset.isNavCenterDisplay = 1 - is_nav_center_display
 
 }
+
+/* 
+function bodyScroll(event) {
+    event.preventDefault();
+}
+
+function scrControl(t,e) {
+    if (t == 0) { //禁止滚动
+        e.addEventListener('scroll', this.bodyScroll, { passive: false });
+    } else if (t == 1) { //开启滚动
+        e.removeEventListener('scroll', this.bodyScroll, { passive: false });
+    }
+} */
