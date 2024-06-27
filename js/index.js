28dd6cc6a3b4a8f1eafdb168515a66d53190b8dc
zjw2018ss <zjw2018ss@gaiml.com>
2024-06-28 04:55:56 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -153,7 +153,7 @@ function get_path() {
                             type: 1,
                             text: "较大更新，出问题及时反馈，谢谢。<hr>马原（3）+习思想（3）=病理（4）+医学分子生物学D(1)+大学体育4(1)<hr>  <a href='demo.html' target='_blank'>查看教程</a>&nbsp; &nbsp; &nbsp; &nbsp; <a href='https://f.wps.cn/g/zMpvWD5Q' target='_blank'>问题反馈</a>",
                             stay: false,
-                            time: 3,
+                            time: 5,
                             position: "bottom"
                         })
                     }, 2000)
